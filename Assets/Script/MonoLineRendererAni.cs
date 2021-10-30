bb5efb6833f80ba19eb1f3d8b485eac35d0f1367
Whimsy Elysium Duke <whimsyduke@163.com>
2021-10-30 11:07:37 +0800

    WIP

---
+++ Assets/Script/MonoLineRendererAni.cs
@@ -0,0 +1,277 @@
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(CustomSimulationSpaceAttribute))]
+public sealed class CustomSimulationSpaceDrawer : PropertyDrawer
+{
+    // Draw the property inside the given rect
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (property.serializedObject.FindProperty("simulationSpace").enumValueIndex == 2)
+            EditorGUI.ObjectField(position, property, label);
+    }
+}
+#endif
+
+public class CustomSimulationSpaceAttribute : PropertyAttribute { }
+
+//[ExecuteInEditMode]
+public class MonoLineRendererAni : MonoBehaviour
+{
+    //相对之前的版本，10000次循环world由140ms缩短至100ms, local由135ms缩短至86ms
+    //在custom模式下，原版本210ms，此版本210ms，维持不变
+    [SerializeField, Header("节点数量"), Tooltip("彩条包含的节点总数。")]
+    private int LineNodeCount = 20;
+    [SerializeField, Header("尾部偏移"), Tooltip("在静止无抖动情况下，彩条尾部的位置（0,0,z）代表彩条长度为z。")]
+    private Vector3 LineTailOffset;
+    [SerializeField, Header("跟随速度"), Tooltip("不考虑抖动情况下，因头部移动使彩条拉长时，彩条缩回的速度（每秒缩回百分比），每个轴的值小于等于0意味着缩回速度无限大，彩条在此方向上不可拉伸，不可形变；大于等于100意味着不会缩回仅受“最大长度”影响。")]
+    private Vector3 FollowSpeed = new Vector3(3,3,3);
+    //[SerializeField, Header("最大长度"), Tooltip("在静止无抖动情况下，彩条的最大长度，不会短于尾部偏移带来的长度变化。")]
+    //private Vector3 LineMaxLength = Vector3.zero;
+    [SerializeField]
+    private ParticleSystemSimulationSpace simulationSpace = ParticleSystemSimulationSpace.World;
+    [SerializeField]
+    [CustomSimulationSpace]
+    private Transform customSimulationSpace;
+    [SerializeField]
+    [Tooltip("默认值0代表不使用固定频率更新，更新频率跟Unity的Update保持一致；填非0值，无论实际帧率多少，Tick频率都是一样的")]
+    private uint _fixedTickRate = 0;
+
+    private LineRenderer lineR;
+    private Vector3[] positions;
+    private Matrix4x4? matCustomSpaceWL = null;
+    private Matrix4x4? matCustomSpaceOffset = null;
+
+    private bool _need_reset = false;
+    private bool bWaveOffset = false;
+    private bool bPointOffsetRandom = false;
+    private Vector3 noiseVec;
+    private Matrix4x4 space;
+
+    private float fixedTickStep { get { return _fixedTickRate == 0 ? 0f : 1f / (float)_fixedTickRate; } }
+    private float _timePassed;
+    //高于此值的话我们认为这个时间累计是异常的，需要舍弃
+    private const float TIME_PASSED_THRESHOLD = 1f;
+
+    void OnEnable()
+    {
+        _need_reset = true;
+        _timePassed = 0f;
+    }
+
+    void OnDisable()
+    {
+        _need_reset = true;
+        if (lineR != null)
+        {
+            lineR.positionCount = 2;
+            lineR.SetPosition(0, Vector3.zero);
+            lineR.SetPosition(1, Vector3.zero);
+        }
+    }
+
+    private void reset()
+    {
+        lineR = GetComponent<LineRenderer>();
+        lineR.useWorldSpace = simulationSpace != ParticleSystemSimulationSpace.Local;
+
+        lineR.positionCount = LineNodeCount;
+        positions = new Vector3[LineNodeCount];
+
+        if (simulationSpace != ParticleSystemSimulationSpace.Local)
+            space = transform.localToWorldMatrix;
+        if (simulationSpace == ParticleSystemSimulationSpace.Custom)
+            SetCustomSpaceOffset();
+
+        for (int i = 0; i < LineNodeCount; i++)
+        {
+            if (i == 0)
+            {
+                if (simulationSpace == ParticleSystemSimulationSpace.Local)
+                    positions[i] = Vector3.zero;
+                else
+                    positions[i] = new Vector3(space.m03, space.m13, space.m23);
+            }
+            else
+            {
+                if (simulationSpace == ParticleSystemSimulationSpace.Local)
+                    positions[i] = positions[0] + (LineTailOffset / (LineNodeCount - 1)) * i;
+                else
+                    positions[i] = positions[0] + space.MultiplyVector((LineTailOffset / (LineNodeCount - 1)) * i);
+            }
+        }
+        lineR.SetPositions(positions);
+    }
+
+    private void Update()
+    {
+        if (_fixedTickRate == 0)
+            UpdatePoints(Time.deltaTime);
+        else
+            FixedStepUpdate();
+
+    }
+
+    /// <summary>
+    /// 如果直接在Update里做，不同帧率的表现是不一样的，我们需要在逻辑上固定帧率
+    /// </summary>
+    private void FixedStepUpdate()
+    {
+        _timePassed += Time.deltaTime;
+        //不应该出现大于阈值的情况，如果大于阈值，就reset一波
+        if (_timePassed > TIME_PASSED_THRESHOLD)
+        {
+            _timePassed = 0;
+            _need_reset = true;
+            UpdatePoints(fixedTickStep);
+        }
+        else
+        {
+            while (_timePassed >= fixedTickStep)
+            {
+                _timePassed -= fixedTickStep;
+                UpdatePoints(fixedTickStep);
+            }
+        }
+    }
+
+    private Vector3 Lerp(Vector3 a, Vector3 b, Vector3 t)
+    {
+        double x = a.x + (b.x - a.x) * (double)t.x;
+        double y = a.y + (b.y - a.y) * (double)t.y;
+        double z = a.z + (b.z - a.z) * (double)t.z;
+        return new Vector3((float)x, (float)y, (float)z);
+    }
+
+    private void UpdatePoints(float deltaTime)
+    {
+        if (_need_reset)
+        {
+            reset();
+            _need_reset = false;
+        }
+
+        lineR.useWorldSpace = simulationSpace != ParticleSystemSimulationSpace.Local;
+
+        if (simulationSpace != ParticleSystemSimulationSpace.Local)
+            space = transform.localToWorldMatrix;
+        if (simulationSpace == ParticleSystemSimulationSpace.Custom)
+            SetCustomSpaceOffset();
+
+        if (positions.Length != LineNodeCount)
+        {
+            positions = new Vector3[LineNodeCount];
+            lineR.positionCount = LineNodeCount;
+        }
+
+        Vector3 off = LineTailOffset / (LineNodeCount - 1);
+
+        #region 处理各方向跟随速度
+        Vector3 followSpeed;
+        if (FollowSpeed.x < 0 || FollowSpeed.x > 100)
+        {
+            followSpeed.x = 1;
+        }
+        else
+        {
+            followSpeed.x = deltaTime * FollowSpeed.x / 100;
+            if (followSpeed.x > 1)
+            {
+                followSpeed.x = 1;
+            }
+        }
+        if (FollowSpeed.y < 0 || FollowSpeed.y > 100)
+        {
+            followSpeed.y = 1;
+        }
+        else
+        {
+            followSpeed.y = deltaTime * FollowSpeed.y / 100;
+            if (followSpeed.y > 1)
+            {
+                followSpeed.y = 1;
+            }
+        }
+        if (FollowSpeed.z < 0 || FollowSpeed.z > 100)
+        {
+            followSpeed.z = 1;
+        }
+        else
+        {
+            followSpeed.z = deltaTime * FollowSpeed.z / 100;
+            if (followSpeed.z > 1)
+            {
+                followSpeed.z = 1;
+            }
+        }
+        #endregion 处理各方向跟随速度
+
+        for (int i = 0; i < LineNodeCount; i++)
+        {
+            if (i == 0)
+            {
+                //第一个点的坐标
+                if (simulationSpace == ParticleSystemSimulationSpace.Local)
+                    positions[i] = Vector3.zero;
+                else
+                    positions[i] = new Vector3(space.m03, space.m13, space.m23);
+            }
+            else
+            {
+                switch (simulationSpace)
+                {
+                    case ParticleSystemSimulationSpace.Local:
+                        positions[i] = Lerp(positions[i], positions[i - 1] + off, followSpeed);
+                        if (bPointOffsetRandom)
+                            positions[i] += noiseVec;
+                        break;
+
+                    case ParticleSystemSimulationSpace.World:
+                        positions[i] = Lerp(positions[i], positions[i - 1] + space.MultiplyVector(off), followSpeed);
+                        if (bPointOffsetRandom)
+                            positions[i] += space.MultiplyVector(noiseVec);
+                        break;
+
+                    case ParticleSystemSimulationSpace.Custom:
+                        if (matCustomSpaceOffset != null)
+                        {
+                            //根据参考transform的移动来运动
+                            positions[i] = matCustomSpaceOffset.Value.MultiplyPoint3x4(positions[i]);
+                        }
+                        positions[i] = Lerp(positions[i], positions[i - 1] + space.MultiplyVector(off), followSpeed);
+                        if (bPointOffsetRandom)
+                            positions[i] += space.MultiplyVector(noiseVec);
+                        break;
+                }
+            }
+        }
+        lineR.SetPositions(positions);
+    }
+
+    //以下函数只有在custom模式下起作用
+    private void SetCustomSpaceOffset()
+    {
+        //custom模式但没有添加参考transform, 此时相当于world模式
+        if (customSimulationSpace == null)
+        {
+            matCustomSpaceWL = null;
+            matCustomSpaceOffset = null;
+            return;
+        }
+
+        //custom模式且添加了参考transform
+        if (matCustomSpaceWL == null)
+        {
+            //第一次循环，matCustomSpaceWL没有值
+            matCustomSpaceOffset = null;
+        }
+        else
+        {
+            //不是第一次循环，matCustomSpaceWL有值
+            matCustomSpaceOffset = customSimulationSpace.localToWorldMatrix * matCustomSpaceWL;
+        }
+        matCustomSpaceWL = customSimulationSpace.worldToLocalMatrix;
+    }
+}
--- Assets/Script/MoveableLineRenderer.cs
+++ Assets/Script/MoveableLineRenderer.cs
@@ -8,35 +8,46 @@ using UnityEditor;
 
 namespace Assets.MoveableLineRenderer.Scripts
 {
-	[ExecuteInEditMode]
+	//[ExecuteInEditMode]
 	internal sealed class MoveableLineRenderer : MonoBehaviour
 	{
 		//public GameObject LineRendererPrefab;
-		[Tooltip("默认值0代表不使用固定频率更新，更新频率跟Unity的Update保持一致")]
+		[Header("默认值 0 更新频率跟Update一致")]
 		[SerializeField]
 		private uint _fixedTickRate = 0;
-		[Header("最小间距"), Range(0.01f, 0.2f)]
+		[Header("顶点插值段数"), Range(1, 10)]
 		[SerializeField]
-		private float MinVertexDistance = 0.1f;
+		private int Tween = 1;
 		[Header("节点寿命")]
 		[SerializeField]
 		private float LifeTime = 1f;
 		[Header("最大长度")]
 		[SerializeField]
 		private float MaxLength = 1f;
-		[Header("计算精度(越低越准确)"), Range(1, 5)]
-		[SerializeField]
-		private int Resolution = 1;
 		[Header("节点速度(Z方向)")]
 		[SerializeField]
 		private float Speed = 1f;
 		[Header("重力")]
 		[SerializeField]
 		private float Gravity = 0f;
+		[Header("跟随速度")]
+		[SerializeField]
+		private float FollowSpeed = 3f;
 
-		//控制扰动
-		//[Header("启用长度")]
-		//public bool Keeplength = false;
+
+		private enum NoiseType
+		{
+			None,
+			PerlinNoise,
+			RandomNoise,
+			Sin
+		}
+		[Header("扰动类型")]
+		[SerializeField]
+		private NoiseType NoiseMode = NoiseType.None;
+
+
+
 		[Header("节点速度扰动(Z方向)")]
 		public float SpeedNoise = 0f;
 		[Header("振幅(X方向)")]
@@ -45,15 +56,18 @@ namespace Assets.MoveableLineRenderer.Scripts
 		public float AmplitudeY = 0f;
 		[Header("频率")]
 		public float Frequency = 0f;
+		[Header("扰动渐增百分百"), Tooltip("从0开始，每个节点的扰动影响的增加比例。超过100%后固定为100。")]
+		public float NoiseScale = 0;
 
 		//频率关联速度
 
 		private LineRenderer _lineRenderer;
-		private Point[] _points;
-		private int _pointsCount;
-		private float FixedMinVertexDistance;
+		private List<Point> points;
 		private float FixedLifeTime;
-		private float fixedTickStep { get { return _fixedTickRate == 0 ? Time.deltaTime : 1f / (float)_fixedTickRate; } }
+		private float fixedTickStep { get { return _fixedTickRate == 0 ? Time.fixedDeltaTime : 1f / (float)_fixedTickRate; } }
+		private float timePassed;
+		//高于此值的话我们认为这个时间累计是异常的，需要舍弃
+		private const float TIME_PASSED_THRESHOLD = 1f;
 
 		private void Start()
 		{
@@ -77,178 +91,195 @@ namespace Assets.MoveableLineRenderer.Scripts
 				_lineRenderer.widthMultiplier = 0.5f;
 				//_lineRenderer.material = new Material(Shader.Find("Standard Assets/Shaders/UnityBuiltin/Legacy/Normal-Bumped"));
 			}
-
-			_points = new Point[100];
-
+			points = new List<Point>();
+			timePassed = 0f;
 		}
 
-		private void Update()
+		private void FixedUpdate()
 		{
+			//修正帧率对最小间隔和lifeTime的影响。
+			FixedLifeTime = LifeTime / fixedTickStep * Time.deltaTime;
+			if (_fixedTickRate == 0)
+				UpdatePoints(Time.fixedDeltaTime);
+			else
+				FixedStepUpdate();
+		}
 
-			if (LifeTime <= 0)
-				LifeTime = 0.1f;
+		/// <summary>
+		/// 如果直接在Update里做，不同帧率的表现是不一样的，我们需要在逻辑上固定帧率
+		/// </summary>
+		private void FixedStepUpdate()
+		{
+			timePassed += Time.deltaTime;
+			//不应该出现大于阈值的情况，如果大于阈值，就reset一波
+			if (timePassed > TIME_PASSED_THRESHOLD)
+			{
+				timePassed = 0;
+				UpdatePoints(fixedTickStep);
+			}
+			else
+			{
+				while (timePassed >= fixedTickStep)
+				{
+					timePassed -= fixedTickStep;
+					UpdatePoints(fixedTickStep);
+				}
+			}
+		}
 
+		private void UpdatePoints(float deltaTime)
+		{
 			RemoveOutdatedPoints();
 
-			//修正帧率对最小间隔和lifeTime的影响。
-			FixedMinVertexDistance = MinVertexDistance * fixedTickStep / Time.deltaTime;
-			FixedLifeTime = LifeTime / fixedTickStep * Time.deltaTime;
+			points.Insert(0, new Point(transform.position));
 
-			if (_pointsCount == 0)
+			float speed = Speed * fixedTickStep;
+
+			// 节点移动
+			points[0].Offset = (-transform.forward) * speed;
+			for (int i = 1; i < points.Count; i++)
 			{
-				_points[_pointsCount++] = new Point(transform.position);
-				_points[_pointsCount++] = new Point(transform.position);
+				points[i].OriginPosition = points[i - 1].FollowPosition;
+				points[i].Offset = (points[i].OriginPosition - points[i - 1].OriginPosition).normalized * speed;
 			}
 
-
-			bool needAdd = false;
-
-			var sqrDistance = (_points[1].Position - transform.position).sqrMagnitude;
-			//需要计算尾巴长度
-			if (sqrDistance > FixedMinVertexDistance * FixedMinVertexDistance)
+			if (Frequency != 0)
 			{
-				needAdd = true;
+				switch (NoiseMode)
+				{
+					case NoiseType.None:
+						ApplyNone();
+						break;
+					case NoiseType.PerlinNoise:
+						ApplyPerlinNoise();
+						break;
+					case NoiseType.RandomNoise:
+						ApplyRandomNoise();
+						break;
+					case NoiseType.Sin:
+						ApplySin();
+						break;
+				}
 			}
-
-			if (needAdd)
-			{
-				if (_pointsCount == _points.Length)
-					System.Array.Resize(ref _points, _points.Length + 50);
-
-				InsertPoint();
-			}
-
-			ApplyTurbulence();
-			Vector3[] curve = MakeSmoothCurve(_points.Where(r => r != null && r.Position != null).Select(r => r.Position).ToArray(), 3.0f);
-
-			_lineRenderer.positionCount = curve.Length;
-
-			//todo
-			_lineRenderer.SetPositions(curve);
-
-		}
-
-		private void InsertPoint()
-		{
-			for (int i = _pointsCount; i > 0; i--)
-				_points[i] = _points[i - 1];
-
-			_points[0] = new Point(transform.position);
-
-			_pointsCount++;
+			_lineRenderer.positionCount = points.Count;
+			_lineRenderer.SetPositions(points.Where(t => t != null).Select(t => t.Position).ToArray());
 		}
 
 		private void RemoveOutdatedPoints()
 		{
-			if (_pointsCount == 0)
+			if (points.Count == 0)
 				return;
 			//物体移动速度太快时，可能直接把点删没了导致溢出，需要注意点的数量。
 			//MaxLength 设置为 0 时跳过，此时拖尾无限长度（只受寿命影响。）
-			for (int i = _pointsCount - 1; i >= 2; i--)
+			int keepCount = points.Count;
+			float length = 0;
+			for (int i = 0; i < points.Count; i++)
 			{
-				var point = _points[i];
-				if (point == null || point.TimeAlive >= FixedLifeTime)
+				if (points[i] == null || points[i].TimeAlive >= FixedLifeTime)
 				{
-					_points[i] = null;
-					_pointsCount--;
+					keepCount = i;
+					break;
 				}
-
-				else
+				if (MaxLength > 0 && i > 0)
 				{
-					//计算是否超出最大长度值。
-					if (MaxLength != 0 && MaxLength <= CurrentLengthSqr())
+					length = length + Vector3.Distance(points[i - 1].Position, points[i].Position);
+					if (length > MaxLength)
 					{
-						_points[i] = null;
-						_pointsCount--;
+						keepCount = i;
+						break;
 					}
 				}
 			}
+			if  (fixedTickStep > 0.1)
+				Debug.Log($"{fixedTickStep}, {Time.deltaTime}");
+			points.RemoveRange(keepCount, points.Count - keepCount);
 		}
 
+		private void NoiseScaleRate(ref float rate)
+        {
+			if (NoiseScale > 100 || NoiseScale < 0)
+            {
+				return;
+            }
+			rate = rate + NoiseScale / 100;
+			if (rate > 1)
+            {
+				rate = 1;
+            }
+			if (rate < 0)
+            {
+				rate = 0;
+            }
+        }
 
-		private void ApplyTurbulence()
+		private void ApplyNone()
 		{
 			//注意不要扰动到起始点，导致曲线端点位置脱离原点。
-			for (int i = _pointsCount - 1; i >= 1; i--)
+			Vector3 noise = new Vector3(0, -Gravity / 100f, 0);
+			for (int i = points.Count - 1; i >= 1; i--)
 			{
-				if (_points[i] == null)
-					continue;
+				points[i].Noise +=  transform.rotation * noise;
+			}
 
+		}
+
+		private void ApplyPerlinNoise()
+		{
+			//注意不要扰动到起始点，导致曲线端点位置脱离原点。
+			for (int i = points.Count - 1; i >= 1; i--)
+			{
 				var sTime = Time.timeSinceLevelLoad * Frequency;
 
-				var pointPosition = _points[i].Position;
+				var pointPosition = points[i].Position;
 
 				float xCoord = pointPosition.x * Frequency / 100f + sTime;
 				float yCoord = pointPosition.y * Frequency / 100f + sTime;
 				float zCoord = pointPosition.z * Frequency / 100f + sTime;
-
-				//_points[i].Position.x += Height;
-				//_points[i].Position.y += Height - Gravity;
-				//_points[i].Position.z += Height;
-
-				//_points[i].Position.x += (Mathf.PerlinNoise(yCoord, zCoord) - 0.5f) * Speed;
-				//_points[i].Position.y += (Mathf.PerlinNoise(xCoord, zCoord) - 0.5f) * Amplitude  + Height - Gravity;
-				//_points[i].Position.z += (Mathf.PerlinNoise(xCoord, yCoord) - 0.5f) * Amplitude;
-				// _points[i].Position.z += Speed*MinVertexDistance/LifeTime;
-
-				//_points[i].Position += transform.localRotation * (-Vector3.forward) * Speed * MinVertexDistance / LifeTime;
-
 				Vector3 noise = new Vector3((Mathf.PerlinNoise(yCoord, zCoord) - 0.5f) * AmplitudeX / 10f, (Mathf.PerlinNoise(xCoord, zCoord) - 0.5f) * AmplitudeY / 10f - Gravity / 100f, (Mathf.PerlinNoise(xCoord, yCoord) - 0.5f) * SpeedNoise / 10f);
-
 				//计算时需要考虑游戏运行帧率。比如30帧对应0.0333f。暴露参数用fixedTickRate 控制。
-				_points[i].Position += transform.rotation * (-Vector3.forward) * Speed * fixedTickStep + transform.rotation * noise;
+				points[i].Noise += transform.rotation * noise;
+			}
+		}
+		private void ApplyRandomNoise()
+		{
+			//注意不要扰动到起始点，导致曲线端点位置脱离原点。
+			Vector3 noiseold = new Vector3(0, 0, 0);
 
+			for (int i = points.Count - 1; i >= 1; i--)
+			{
+				Vector3 noise = new Vector3(Random.Range(-AmplitudeX / 100f, AmplitudeX / 100f), Random.Range(-AmplitudeY / 100f, AmplitudeY / 100f) - Gravity / 100f, Random.Range(-SpeedNoise / 100f, SpeedNoise / 100f));
 
+				points[i].Noise += transform.rotation * noise - transform.rotation * noiseold;
+
+				//每次计算下一个点时，需要把上一次计算的 noise 扣除掉，避免 noise 的累加。
+				noiseold = noise;
 			}
 		}
 
-
-		//计算曲线长度，根据曲线长度与最大长度的比较控制。
-		private float CurrentLengthSqr()
+		private void ApplySin()
 		{
-			float Lengthsqr = 0f;
-			for (int i = 0; i < _pointsCount - Resolution; i = i + Resolution)
+			//注意不要扰动到起始点，导致曲线端点位置脱离原点。
+			float OffsetX = 0;
+			float OffsetY = 0;
+			float second = Time.time - Mathf.Floor(Time.time);
+			float strength = Mathf.Sin(second * Frequency  * Mathf.PI * 2);
+			OffsetX += AmplitudeX * strength;
+			OffsetY += AmplitudeY * strength;
+			Vector3 noise = new Vector3(OffsetX / 10f, OffsetY / 10f - Gravity / 100f, 0);
+			//计算时需要考虑游戏运行帧率。比如30帧对应0.0333f。暴露参数用fixedTickRate 控制。
+			float noiseScale = 0;
+			NoiseScaleRate(ref noiseScale);
+			points[1].Noise = transform.rotation * noise;
+			points[1].NoiseScale = noiseScale;
+			Vector3 addNoise = points[1].Noise;
+			for (int i = 2; i < points.Count; i++)
 			{
-				Lengthsqr = Lengthsqr + (_points[i].Position - _points[i + 1].Position).magnitude;
-
+				Vector3 currentNoise = points[i].Noise;
+				points[i].Noise = addNoise;
+				NoiseScaleRate(ref noiseScale);
+				points[i].NoiseScale = noiseScale;
+				addNoise = currentNoise;
 			}
-			return Lengthsqr;
-		}
-
-
-		public static Vector3[] MakeSmoothCurve(Vector3[] arrayToCurve, float smoothness)
-		{
-			List<Vector3> points;
-			List<Vector3> curvedPoints;
-			int pointsLength = 0;
-			int curvedLength = 0;
-
-			if (smoothness < 1.0f) smoothness = 1.0f;
-
-			pointsLength = arrayToCurve.Length;
-
-			curvedLength = (pointsLength * Mathf.RoundToInt(smoothness)) - 1;
-			curvedPoints = new List<Vector3>(curvedLength);
-
-			float t = 0.0f;
-			for (int pointInTimeOnCurve = 0; pointInTimeOnCurve < curvedLength + 1; pointInTimeOnCurve++)
-			{
-				t = Mathf.InverseLerp(0, curvedLength, pointInTimeOnCurve);
-
-				points = new List<Vector3>(arrayToCurve);
-
-				for (int j = pointsLength - 1; j > 0; j--)
-				{
-					for (int i = 0; i < j; i++)
-					{
-						points[i] = (1 - t) * points[i] + t * points[i + 1];
-					}
-				}
-
-				curvedPoints.Add(points[0]);
-			}
-
-			return (curvedPoints.ToArray());
 		}
 	}
 }
@@ -257,13 +288,38 @@ namespace Assets.MoveableLineRenderer.Scripts
 {
 	internal sealed class Point
 	{
-		public Vector3 Position;
+		public Vector3 Position {
+			get 
+			{
+				if (Noise != null)
+				{
+					return OriginPosition + Noise * NoiseScale;
+				}
+				else
+				{
+					return OriginPosition;
+				}
+			}
+		}
+		public Vector3 FollowPosition
+        {
+			get
+            {
+				return OriginPosition + Offset;
+            }
+        }
 		private readonly float _timeCreated;
+
+		public Vector3 OriginPosition { get; set; } // 节点移动影响
+		public Vector3 Noise { set; get; } // 节点扰动影响
+		public float NoiseScale { set; get; } // 扰动缩放
+		public Vector3 Offset { set; get; } // 跟随偏移
 
 		public Point(Vector3 position)
 		{
-			Position = position;
+			OriginPosition = position;
 			_timeCreated = Time.time;
+			NoiseScale = 1;
 		}
 
 		public float TimeAlive
